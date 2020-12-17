392970b32688f9e6178303e5116d2902d222be50
Alex Sircu <alex.sricu@gmail.com>
2020-12-17 19:48:25 +0100

    Prova del bonus

--- js/script.js
+++ js/script.js
@@ -2,7 +2,9 @@ var app = new Vue(
   {
     el: "#root",
     data: {
-      albumArray: null
+      albumArray: [],
+      newGenre: [],
+      selectedGenre: ''
 
     },
     mounted: function() {
@@ -11,8 +13,26 @@ var app = new Vue(
         .get('https://flynn.boolean.careers/exercises/api/array/music')
         .then(function (album) {
           myThis.albumArray = album.data.response;
+
+          for (var i = 0; i < myThis.albumArray.length; i++) {
+            if (!myThis.newGenre.includes(album.data.response[i].genre)) {
+                myThis.newGenre.push(album.data.response[i].genre);
+            }
+          }
         }
       );
     }
+    // computed: {
+    //   selectGenre: function() {
+    //     if (this.selectedGenre == "") {
+    //       return this.albumArray;
+    //     } else {
+    //       const filteredArray = this.albumArray.filter((element) => {
+    //         return element.genre == this.selectedGenre;
+    //       });
+    //       return filteredArray;
+    //     }
+    //   }
+    // }
   }
 )
